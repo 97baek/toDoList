cf925f0b17f7eaecf30b0125d9672393517bcb93
97baek <73068489+97baek@users.noreply.github.com>
2020-11-01 18:00:02 +0900

    add weather api

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
 const COORDS = "coords";
+const API_KEY = "67bf7bcad177927d9d5d592ac6dd4b17";
+
+function getWeather(lat, lng) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=${API_KEY}&units=metric`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSuccess(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -27,6 +43,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
